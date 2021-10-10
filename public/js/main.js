4b518b64c38528a8ec4eb31d0af5ac16720eab0e
herediat <ts.heredia@outlook.com>
2021-10-10 05:03:56 +0200

    exos done

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+// // Exercice1
+// let prenom = ["Heredia", "philippe", "ayhan", "elias"];
+// console.log(prenom);
+
+// console.log(prenom.indexOf('elias'));
+// console.log(prenom[3]);
+// console.log(prenom.length);
+
+// console.log("_______");
+
+// Exercice2
+
+// let prenom =  [ "philippe"];
+// console.log(prenom);
+// prenom.push('ballon');
+// prenom.push('john');
+// console.log(prenom);
+
+// console.log("_______");
+
+// // Exercice3
+// let coding = [];
+// coding.push('ayhan');
+// console.log(`Ayhan est arrvié à 8h43`);
+// coding.push('bene');
+// console.log(`Bene est arrvié à 8h44 et met ses notes  à jours`);
+// coding.push('louise', 'jamila');
+// console.log(`louise et jamila sont arrviées à 8h45 `);
+// coding.push('seb', 'adame');
+// console.log('seb et adame sont rentré mais ils voient nicolas et decident de sortir');
+// console.log(coding);
+// coding.splice(coding.indexOf('seb'), 2)
+// console.log(coding);
+// coding.push('dawid', "malo");
+// coding.push('alex');
+// coding.splice(coding.indexOf('alex'),1);
+// coding.push('maxence', 'mohamed');
+
+// //Mettre en MAJ
+// coding[coding.indexOf('maxence')] = coding[coding.indexOf('maxence')].toUpperCase()
+// coding[coding.indexOf('louise')] = coding[coding.indexOf('louise')].toUpperCase()
+
+// coding.push('saliou');
+// coding.push('zulma', "maximilien");
+// coding.push('natchez');
+// coding.push('zak', 'ayoub');
+// coding.splice(coding.indexOf('zak'),2)
+// coding.push('achraf');
+// coding.push('abder');
+// console.log(coding);
+
+// console.log("_______");
+// // BONUS
+// let myTab = ['test', 'test2', "Aaaaaabc"];
+
+
